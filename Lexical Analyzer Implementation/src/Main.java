2b606e6b772f152846e443b9d8fdcd8171f71d94
salehamuhammad <salehamuhammad77@gmail.com>
2026-04-21 00:07:40 +0500

    rem
    changes

--- Lexical Analyzer Implementation/src/Main.java
+++ Lexical Analyzer Implementation/src/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD:A1/23i0661-23i0727-F/src/Main.java
         // Read testInput from test1.lang file
         String testInput = readFile("test5.lang");
         
-=======
-        // Read testInput from test file
-        String testInput = readFile("test\\test_simple.lang");
-
->>>>>>> 8217ee31739612a69127f2fd6fbceaffc93a5549:23i0661-23i0727-F/src/Main.java
         ManualScanner scanner = new ManualScanner(testInput);
         java.util.List<Token> tokens = scanner.scan();
 
@@ -17,27 +11,10 @@ public class Main {
             System.out.println(token);
         }
         
-<<<<<<< HEAD:A1/23i0661-23i0727-F/src/Main.java
         System.out.println("\n=== Lexical Errors ===");
         ErrorHandler errorHandler = scanner.getErrorHandler();
         if (errorHandler.getErrorCount() > 0) {
             errorHandler.print();
-=======
-        System.out.println("\n=== Comments Found ===");
-        java.util.List<ManualScanner.CommentInfo> comments = scanner.getComments();
-        if (comments.isEmpty()) {
-            System.out.println("No comments found.");
-        } else {
-            for (ManualScanner.CommentInfo comment : comments) {
-                System.out.println(comment);
-            }
-        }
-        
-        System.out.println("\n=== Lexical Errors ===");
-        ErrorHandler errorHandler = scanner.getErrorHandler();
-        if (errorHandler.hasErrors()) {
-            errorHandler.printSummary();
->>>>>>> 8217ee31739612a69127f2fd6fbceaffc93a5549:23i0661-23i0727-F/src/Main.java
         } else {
             System.out.println("No errors found.");
         }
@@ -48,11 +25,7 @@ public class Main {
         System.out.println("\n=== Symbol Table ===");
         // Use the symbol table populated by the scanner
         SymbolTable symbolTable = scanner.getSymbolTable();
-<<<<<<< HEAD:A1/23i0661-23i0727-F/src/Main.java
         symbolTable.print();
-=======
-        symbolTable.printTable();
->>>>>>> 8217ee31739612a69127f2fd6fbceaffc93a5549:23i0661-23i0727-F/src/Main.java
     }
 
     private static String readFile(String filename) {
